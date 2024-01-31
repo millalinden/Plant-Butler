b92e9aab902f396a1b1a549b280e0665bf1fd937
Marianna <maryanarez@gmail.com>
2024-01-31 13:52:45 +0100

    LandingPage.jsx to main

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -1,3 +1,23 @@
 // -----------------------------------------------------------------------------
 // LANDING PAGE WHERE THE USER CAN GO TO EITHER SHOP OR Q&A
 // -----------------------------------------------------------------------------
+
+import styles from "./LandingPage.module.css";
+function LandingPage() {
+  return (
+    <div>
+      <main className={styles.landing}>
+        <h1>Plants Butler</h1>
+        <br />
+        <h2>Hello plant lover! </h2>
+        <h2>What do you want to do today?</h2>
+        <nav className={styles.landingBtnContainer}>
+          <button className={styles.landingBtn}>Shop</button>
+          <button className={styles.landingBtn}>Plant detective</button>
+          <button className={styles.landingBtn}>Keep in touch</button>
+        </nav>
+      </main>
+    </div>
+  );
+}
+export default LandingPage;

82198de23992045c0db9837884ed5c0e07e56807
petra milicevic <milicevicpetra7@gmail.com>
2024-02-05 10:01:54 +0100

    scroll button and product filter

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,9 @@ import LandingPage from "./Pages/LandingPage/LandingPage";
 import PlantDetective from "./Pages/PlantDetective/PlantDetective";
 import ProductPage from "./Pages/ProductPage/ProductPage";
 
+import ProductData from "./productData.json";
+import ProductFilter from "./Components/productFilter/productFilter";
+
 function App() {
   return (
     <div className="app-container">
@@ -23,7 +26,9 @@ function App() {
             <Route path="/" element={<LandingPage />} />
             <Route path="/" element={<Navbar />}>
               <Route path="/Shop" element={<Shop />} />
+
               <Route path="/CartPage" element={<CartPage />} />
+
               <Route path="/CheckoutPage" element={<CheckoutPage />} />
               <Route path="/Footer" element={<Footer />} />
               <Route path="/AboutUs" element={<AboutUs />} />
--- src/Pages/Shop/Shop.jsx
+++ src/Pages/Shop/Shop.jsx
@@ -1,23 +1,55 @@
 // -----------------------------------------------------------------------------
 // STRUCTURE FOR SHOP AND THE "CONTAINER" WHERE THE PRODUCT CARDS ARE ADDED
 // -----------------------------------------------------------------------------
-import React from "react";
+
+import React, { useState, useEffect } from "react";
 import { PRODUCTS } from "../../products.js";
 import { Product } from "../../Components/Product/Product.jsx";
 import styles from "./Shop.module.css";
 import Footer from "../../Components/Footer/Footer";
+import ScrollToTopButton from "../../Components/ScrolltotopButton/Scroll-to-top-Button.jsx";
+import ProductFilter from "../../Components/productFilter/productFilter.jsx";
 
 const Shop = () => {
+  const [showTopBtn, setShowTopBtn] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 800) {
+        setShowTopBtn(true);
+      } else {
+        setShowTopBtn(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const goToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
   return (
     <div className={styles.shop}>
+      <div className="productFilter">
+        <ProductFilter />
+      </div>
       <div className={styles.shopTitle}></div>
       <div className="products">
         {PRODUCTS.map((product) => (
           <div key={product.id}>
-              <Product data={product} />
+            <Product data={product} />
           </div>
         ))}
       </div>
+      {showTopBtn && <ScrollToTopButton onClick={goToTop} />}
       <Footer />
     </div>
   );

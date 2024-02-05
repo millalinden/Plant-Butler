310f2b18c7eacb672d22c465e6096f1bbd5e6192
Marianna <maryanarez@gmail.com>
2024-02-05 10:27:22 +0100

    Styling of whole App, LandingPage, AboutUs. Linked fonts to index.html

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import CartPage from "./Pages/CartPage/CartPage";
 import CheckoutPage from "./Pages/CheckoutPage/CheckoutPage";
 import AboutUs from "./Components/AboutUs/AboutUs";
 import Footer from "./Components/Footer/Footer";
-import "./Components/Navbar/Navbar.css";
+import "./Components/Navbar/Navbar.module.css";
 import Navbar from "./Components/Navbar/Navbar";
 import Shop from "./Pages/Shop/Shop";
 import LandingPage from "./Pages/LandingPage/LandingPage";
@@ -16,23 +16,25 @@ import ProductPage from "./Pages/ProductPage/ProductPage";
 
 function App() {
   return (
-    <div className="app-container">
-      <ShopContextProvider>
-        <Router>
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/" element={<Navbar />}>
-              <Route path="/Shop" element={<Shop />} />
-              <Route path="/CartPage" element={<CartPage />} />
-              <Route path="/CheckoutPage" element={<CheckoutPage />} />
-              <Route path="/Footer" element={<Footer />} />
-              <Route path="/AboutUs" element={<AboutUs />} />
-              <Route path="/PlantDetective" element={<PlantDetective />} />
-              <Route path="/product/:id" element={<ProductPage />} />
-            </Route>
-          </Routes>
-        </Router>
-      </ShopContextProvider>
+    <div className="app-wrapper">
+      <div className="app-container">
+        <ShopContextProvider>
+          <Router>
+            <Routes>
+              <Route path="/" element={<LandingPage />} />
+              <Route path="/" element={<Navbar />}>
+                <Route path="/Shop" element={<Shop />} />
+                <Route path="/CartPage" element={<CartPage />} />
+                <Route path="/CheckoutPage" element={<CheckoutPage />} />
+                <Route path="/Footer" element={<Footer />} />
+                <Route path="/AboutUs" element={<AboutUs />} />
+                <Route path="/PlantDetective" element={<PlantDetective />} />
+                <Route path="/product/:id" element={<ProductPage />} />
+              </Route>
+            </Routes>
+          </Router>
+        </ShopContextProvider>
+      </div>
     </div>
   );
 }
